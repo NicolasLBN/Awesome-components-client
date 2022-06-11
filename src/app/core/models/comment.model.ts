403d38096b642d60fe336f197cda004b631f3e76
nicolas <niko83136@gmail.com>
2022-06-11 11:29:20 +0200

    post and comment models created

---
+++ src/app/core/models/comment.model.ts
@@ -0,0 +1,6 @@
+export class Comment {
+    id!: number;
+    serId!: number; 
+    comment!: string;
+    createdDate!: String;
+}
---
+++ src/app/social-media/models/post.model.ts
@@ -0,0 +1,9 @@
+export class Post {
+    id!: number;
+    userId!: number;
+    title!: string;
+    createdDate!: string; 
+    imageUrl!: string; 
+    content!: string;
+    comments!: Comment[]
+}
