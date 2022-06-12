74e267529d3b2f20aaa84e46bec5bb365cb16edb
nicolas <niko83136@gmail.com>
2022-06-12 13:19:32 +0200

    Initialize complex-form

---
+++ src/app/complex-form/components/complex-form/complex-form.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-complex-form',
+  templateUrl: './complex-form.component.html',
+  styleUrls: ['./complex-form.component.scss']
+})
+export class ComplexFormComponent implements OnInit {
+
+  mainForm!: FormGroup;
+  personalInfoForm!: FormGroup;
+  contactPreferenceCtrl!: FormControl;
+  phoneCtrl!: FormControl;
+  emailCtrl!: FormControl;
+  confirmEmailCtrl!: FormControl;
+  emailForm!: FormGroup;
+  passwordCtrl!: FormControl;
+  confirmPasswordCtrl!: FormControl;
+  loginInfoForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.initFormControls();
+    this.initMainForm();
+  }
+
+  private initFormControls(): void {
+    this.personalInfoForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+    });
+    this.contactPreferenceCtrl = this.formBuilder.control('email');
+    this.phoneCtrl = this.formBuilder.control('');
+    this.emailCtrl = this.formBuilder.control('');
+    this.confirmEmailCtrl = this.formBuilder.control('');
+    this.emailForm = this.formBuilder.group({
+      email: this.emailCtrl,
+      confirm: this.confirmEmailCtrl
+    });
+    this.passwordCtrl = this.formBuilder.control('', Validators.required);
+    this.confirmPasswordCtrl = this.formBuilder.control('', Validators.required);
+    this.loginInfoForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: this.passwordCtrl,
+      confirmPassword: this.confirmPasswordCtrl
+    });
+  }
+
+  initMainForm(): void {
+
+  }
+}
---
+++ src/app/complex-form/services/complex-form.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable, of } from "rxjs";
+import { catchError, delay, mapTo } from "rxjs/operators";
+import { ComplexFormValue } from "src/app/core/models/complex-form-value.model";
+import { environment } from "src/environments/environment";
+
+@Injectable()
+export class ComplexFormService {
+  constructor(private http: HttpClient) {}
+
+  saveUserInfo(formValue: ComplexFormValue): Observable<boolean> {
+    return this.http.post(`${environment.apiUrl}/users`, formValue).pipe(
+      mapTo(true),
+      delay(1000),
+      catchError(() => of(false).pipe(
+        delay(1000)
+      ))
+    );
+  }
+}
